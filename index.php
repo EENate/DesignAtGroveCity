a196a5b5bb63a84ad422e626f5ced14da3c792d1
Nate Mucha <nate@eachevery.com>
2016-02-16 19:00:57 -0500

    Initial Commit
    General file structure and beginning HTML and CSS are in place.

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Design at Grove City College</title>
+	
+	<!--LINK TO STYLESHEETS-->
+	<link rel="stylesheet" type="text/css" href="styles/datgcc.css" />
+	<link type="text/css" rel="stylesheet" href="http://fast.fonts.net/cssapi/88b95653-6e26-4d1a-aa40-1b7da1cfe23a.css"/>
+	
+</head>
+<body>
+
+<!--PAGE BORDER AND BACKGROUND ELEMENTS-->
+<div id="pageBorder">
+	<div id="top"></div>
+	<div id="bottom"></div>
+	<div id="right"></div>
+	<div id="left"></div>
+</div>
+<div id="pageBG"></div>
+
+<!--PAGE CONTENT-->
+<section id="mainContent">
+<article id="top">
+	<div class="content">
+		<h1>Design<span>at Grove City College</span></h1>
+	</div>
+</article>
+
+
+</section>
+
+
+</body>
+
+</html>
